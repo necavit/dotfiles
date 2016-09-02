0102082b6b5599446226b6564bce4f7fae3136ad
necavit <gollumdeagol@gmail.com>
2016-09-02 15:10:18 +0200

    updated aliases

--- bin/latexclean.sh
+++ bin/latexclean.sh
@@ -3,12 +3,20 @@
 echo "$(basename $0) will remove the following files:"
 find . -name "*.aux"
 find . -name "*.bbl"
+find . -name "*.bcf"
 find . -name "*.blg"
 find . -name "*.log"
+find . -name "*.run.xml"
 find . -name "*.out"
 
 read -p "Continue? (y/n) " -n 1
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rm *.aux *.bbl *.blg *.log *.out
+  find . -name "*.aux" | xargs rm -f
+  find . -name "*.bbl" | xargs rm -f
+  find . -name "*.bcf" | xargs rm -f
+  find . -name "*.blg" | xargs rm -f
+  find . -name "*.log" | xargs rm -f
+  find . -name "*.run.xml" | xargs rm -f
+  find . -name "*.out" | xargs rm -f
 fi
