6ab3041da1b5e8dca22b0c3213b7b7b7419c3d07
necavit <gollumdeagol@gmail.com>
2017-01-29 17:55:26 +0100

    added status feature for the backup wrapper script

--- bin/backup-extdrive.sh
+++ bin/backup-extdrive.sh
@@ -4,13 +4,14 @@
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
+bold=`tput bold`
 
 function usage {
-  echo "Usage: `basename $0` [-h|--help] [COMMAND [--force]]"
+  echo "${bold}Usage: `basename $0` [-h|--help] [COMMAND [--force]]${reset}"
   echo ""
   echo "  Performs an incremental duplicity based backup as specified in the config file."
-  echo "  Note that, by default, no command is executed without --force. Rather,"
-  echo "  it prints the command that will be executed."
+  echo "  Note that, by default, ${bold}backup${reset} and ${bold}remove-old${reset} commands are not executed without"
+  echo "  specifying the --force. Rather, it prints the command that will be executed."
   echo ""
   echo "  The general config file for this duplicity wrapper is:"
   echo "    $configFile"
@@ -19,16 +20,18 @@ function usage {
   echo "    $includeFileList"
   echo ""
   echo " COMMANDS"
-  echo "    backup"
+  echo "    ${bold}backup${reset}"
   echo "        Performs a backup, as specified in the config file located at $configFile"
-  echo "    remove-old"
+  echo "    ${bold}remove-old${reset}"
   echo "        Removes old backup chains (full and incremental backups), up until the"
   echo "        last full backup and its subsequent incremental sets"
+  echo "    ${bold}status${reset}"
+  echo "        Checks the backup collection status in the configured backup media"
   echo ""
   echo " OPTIONS"
-  echo "    --force"
+  echo "    ${bold}--force${reset}"
   echo "        Disables dry-run and actually executes the command"
-  echo "    -h|--help"
+  echo "    ${bold}-h|--help${reset}"
   echo "        Displays this usage page and exits"
   exit 1
 }
@@ -36,7 +39,7 @@ function usage {
 function backup {
   dryRun="--dry-run"
   if [[ "$1" = "--force" ]]; then
-    echo "${red}!!WARNING!!${reset}"
+    echo "${bold}${red}!!! WARNING !!! WARNING !!! WARNING !!!${reset}"
     read -p "This will perform the backup. Continue? (y/n) " -n 1
     echo ""
     if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -55,7 +58,7 @@ function backup {
 function removeOld {
   dryRun=""
   if [[ "$1" = "--force" ]]; then
-    echo "${red}!!WARNING!!${reset}"
+    echo "${bold}${red}!!! WARNING !!! WARNING !!! WARNING !!!${reset}"
     read -p "This will remove old backups. Continue? (y/n) " -n 1
   	echo ""
   	if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -69,6 +72,13 @@ function removeOld {
   echo "${green}executing: $rmoldcmd${reset}"
   echo ""
   $rmoldcmd
+}
+
+function checkStatus {
+  statuscmd="duplicity collection-status $targetUrl"
+  echo "${green}executing: $statuscmd${reset}"
+  echo ""
+  $statuscmd
 }
 
 #echo "Usage: `basename $0` [-h|--help] [COMMAND [--force]]"
@@ -90,6 +100,8 @@ if [[ $(id -u) -ne 0 ]]; then
   elif [[ "$1" = "remove-old" ]]; then
     shift
     removeOld "$@"
+  elif [[ "$1" = "status" ]]; then
+    checkStatus
   else
     echo "Error: command not recognised: $1"
   fi
