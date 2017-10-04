ffe223c9b237b7999d9ea28cddac304e1bc7071b
necavit <davmarod@gmail.com>
2017-10-04 13:03:06 +0200

    added a python script to toggle network connections up or down

---
+++ bin/nmcli-toggle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import json
+import sys
+import subprocess
+
+def nmcli_toggle(args):
+	conn_id = args.connection
+	active = subprocess.check_output(['nmcli', 'connection', 'show', '--active'])
+	up_down = ('up', 'down')[conn_id in active]
+	subprocess.call(['nmcli', 'connection', up_down, 'id', conn_id])
+
+
+if __name__ == '__main__':
+  # top level argument parser
+  parser = argparse.ArgumentParser(
+                      description='Toggles a Network connection using nmcli.')
+  parser.add_argument('connection',
+                      help='the connection to be toggled')
+  args = parser.parse_args()
+
+  # main procedure
+  nmcli_toggle(args)
